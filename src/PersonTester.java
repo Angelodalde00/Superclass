ef18cba8a9baba098e97f5a73442fadd21177ffe
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-24 16:51:08 +0800

    Adding superclass

---
+++ src/PersonTester.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class PersonTester {
+    
+ 
+
+public static void main(String[] args)
+  {
+ Person a = new Person("Edoy", 2001);
+    Student b = new Student("lester", 2003, "BSIT");
+    Instructor c = new Instructor("josip", 2001, 500);
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+}
+}
+
